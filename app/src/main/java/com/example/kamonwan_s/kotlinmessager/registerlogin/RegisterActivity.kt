464a253f5f15b9c7fc60faa3ed1cd1a501696f76
kamonwans <kamonwan294@gmail.com>
2018-10-28 13:32:36 +0700

    item chat

--- app/src/main/java/com/example/kamonwan_s/kotlinmessager/registerlogin/RegisterActivity.kt
+++ app/src/main/java/com/example/kamonwan_s/kotlinmessager/registerlogin/RegisterActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
+import com.example.kamonwan_s.kotlinmessager.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -114,7 +115,4 @@ class RegisterActivity : AppCompatActivity() {
 
                 }
     }
-}
-class User(val uid: String,val username : String ,val profileImageUrl:String){
-    constructor():this("","","")
 }
